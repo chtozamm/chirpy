acbe8d54134d1702c065ae34dc89df5a3cb9fbdc
Mark M <chtozamm@gmail.com>
2025-02-13 12:25:06 +0300

    Complete Chapter 3: Architecture

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	apiCfg := apiConfig{atomic.Int32{}}
+
 	mux := http.NewServeMux()
 
 	server := http.Server{
@@ -13,7 +28,40 @@ func main() {
 		Handler: mux,
 	}
 
-	// mux.HandleFunc("/", http.NotFound)
+	// mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte("OK"))
+	})
+	// mux.HandleFunc("GET /api/metrics", func(w http.ResponseWriter, r *http.Request) {
+	// 	hits := apiCfg.fileserverHits.Load()
+	// 	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+	// })
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.New("").Parse(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited {{.}} times!</p>
+  </body>
+</html>`)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		hits := apiCfg.fileserverHits.Load()
+
+		w.Header().Set("Content-Type", "text/html")
+		t.Execute(w, hits)
+		// w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+	})
+	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Store(0)
+		// w.WriteHeader(http.StatusOK)
+	})
 
 	log.Fatal(server.ListenAndServe())
 }
+
